4e8e3106aa670b83b6631d7235173103cb56fc3a
TheAssassin <theassassin@assassinate-you.net>
2020-06-12 17:36:52 +0200

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,96 @@
+import os
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Blue Nebula'
+copyright = '2020, Blue Nebula Team'
+author = 'Blue Nebula Team'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.githubpages',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    "display_version": True,
+    "sticky_navigation": True,
+    "includehidden": True,
+    "collapse_navigation": True,
+    "titles_only": True,
+    "prev_next_buttons_location": "both",
+}
+
+# try to fetch current Git commit ID from the environment
+commit = os.environ.get("TRAVIS_COMMIT", os.environ.get("GIT_COMMIT", None))
+
+# if this is not possible for some reason, try to fetch it via the git command
+if not commit:
+    import subprocess
+    try:
+        commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().split()[0]
+    except subprocess.CalledProcessError:
+        commit = "<not available>"
+
+# make sure to use short commit
+commit = commit[:7]
+
+html_context = {
+    "display_github": True,
+    "github_user": "blue-nebula",
+    "github_repo": "docs",
+    "github_version": "master/source/",
+    "commit": commit,
+}
+
+#html_favicon = "_static/favicon.ico"
+
---
+++ source/index.rst
@@ -0,0 +1,15 @@
+.. Blue Nebula documentation master file, created by
+   sphinx-quickstart on Fri Jun 12 16:06:15 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to the Blue Nebula documentation
+========================================
+
+These docs contain information for users, content creators (e.g., mappers), developers and anybody else using or working with Blue Nebula.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
